4185cb1cdc465d1244d594562e46a7496ed9cea1
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2024-04-08 17:33:58 +0530

    adding logging

--- loggin.go
+++ loggin.go
@@ -1,4 +1,9 @@
 package main
+import (
+  "context",
+  "time"
+  "github.com/sirupsen/logrus"
+)
 
 type loggingService struct {
 
@@ -8,8 +13,22 @@ type loggingService struct {
 }
 
 
+func NewLoggingService(next PriceFetcher) PriceFetcher{
+  return &loggingService {
+    next :next,
+  }
+}
+
+
 func (s *loggingService) FetchPrice(ctx context.Context,ticker string) (price float64,err error) {
   defer func (begin time.Time) {
+    logrus.WithFields(logrus.Fields{
+      "took":time.Since(begin),
+      "err":err,
+      "price":price,
+    }).Info("fetchPrice")
 
   } (time.Now())
+
+  return s.next.FetchPrice(ctx,ticker)
 }
