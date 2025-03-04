5f399c4935acce55bdbe123168bdcf0858bc071b
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2025-03-04 19:19:19 +0530

    creating json encoder and decoder

--- gateway/http_handler.go
+++ gateway/http_handler.go
@@ -1,15 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	pb "github.com/bloodgroup-cplusplus/gRPC_microservices/common/api"
+)
+
 
 type handler struct {
 	// gateway is going to be a layer in service discovery
+	client pb.OrderServiceClient
 
 
 }
 
-func NewHandler () *handler {
-	return &handler{}
+
+func NewHandler (client pb.OrderServiceClient) *handler {
+	return &handler{client}
 }
 
 
@@ -20,5 +27,15 @@ func (h *handler) registerRoutes(mux *http.ServeMux) {
 }
 
 func (h *handler) HandleCreateOrder (w http.ResponseWriter, r *http.Request) {
+	customerID := r.PathValue("customerID")
+	
+
+	h.client.CreateOrder(r.Context(),&pb.CreateOrderRequest{
+		CustomerID:customerID ,
+		Items: items,
+	})
+	
+
+
 
 }
--- gateway/main.go
+++ gateway/main.go
@@ -5,17 +5,29 @@ import (
 	"net/http"
 
 	_ "github.com/joho/godotenv/autoload"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	"github.com/bloodgroup-cplusplus/gRPC_microservices/common"
+	pb "github.com/bloodgroup-cplusplus/gRPC_microservices/common/api"
 )
 
 var (
 	httpAddr = common.EnvString("HTTP_ADDR",":3000")
+	orderServiceAddr = "localhost:3000"
 )
 
 func main() {
+	conn,err := grpc.Dial(orderServiceAddr, grpc.WithTransportCredentials(
+		insecure.NewCredentials()))
+	if err !=nil {
+		log.Fatalf("Failed to dial server: %v",err)
+	}
+	defer conn.Close()
+	log.Println("dialing orders service at",orderServiceAddr)
+	c := pb.NewOrderServiceClient(conn)
 	mux := http.NewServeMux()
-	handler := NewHandler()
+	handler := NewHandler(c)
 	handler.registerRoutes(mux)
 
 	log.Printf("Http server running at %s",httpAddr)
